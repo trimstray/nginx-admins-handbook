6b4ba596e5b37f2a2d799f640dc36ba51f6093db
trimstray <trimstray@gmail.com>
2019-05-26 07:13:56 +0200

    ngx_installer.sh: update description - signed-off-by: trimstray <trimstray@gmail.com>

--- lib/ngx_installer.sh
+++ lib/ngx_installer.sh
@@ -20,7 +20,7 @@
 #
 # License:
 #
-#   htrace.sh, Copyright (C) 2018  Michał Żurawski
+#   ngx_installer.sh, Copyright (C) 2018  Michał Żurawski
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
